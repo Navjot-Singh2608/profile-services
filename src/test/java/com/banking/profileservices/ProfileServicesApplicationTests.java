39e353c01aff50e6d0307a8fc5bc834cf68d12ca
navjotsingh2608 <navjot@argenelec.com>
2023-07-30 00:34:26 -0400

    update

---
+++ src/test/java/com/banking/profileservices/ProfileServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.profileservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
